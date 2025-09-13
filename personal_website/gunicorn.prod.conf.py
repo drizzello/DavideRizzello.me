ad08b79bd1689b171fcf814bcfd7c0e50a5640e2
davide rizzello <daviderizzello@MacBook-Pro-di-davide.local>
2025-09-13 22:39:53 +0200

    - Modified Gunicorn configuration to dynamically set the Django settings module based on the ENV variable (default: dev). - Adjusted WSGI file to use the ENV variable for selecting the appropriate settings module. - Added a newline at the end of the Caddyfile for consistency.

--- personal_website/gunicorn.prod.conf.py
+++ personal_website/gunicorn.prod.conf.py
@@ -1,10 +1,15 @@
 import os
 import multiprocessing
 
-# Settings di produzione
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_website.settings.prod")
+# Legge la variabile ENV (default: dev)
+env = os.getenv("ENV", "dev")
+
+if env == "prod":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_website.settings.prod")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_website.settings.dev")
 
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1  # Formula consigliata
-reload = False
+reload = env != "prod"  # In dev autoreload, in prod no
 timeout = 120
--- personal_website/personal_website/wsgi.py
+++ personal_website/personal_website/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for personal_website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_website.settings')
+# Usa ENV=prod o ENV=dev (default: dev)
+env = os.getenv("ENV", "dev")
+
+if env == "prod":
+    settings_module = "personal_website.settings.prod"
+else:
+    settings_module = "personal_website.settings.dev"
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
